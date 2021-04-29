6be61649c36ad3ba4c3f300f6d641a954617946f
jinipk <53362964+jinipk@users.noreply.github.com>
2021-04-29 10:46:54 +0900

    Add files via upload

---
+++ hellohaskell/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for hellohaskell
+
+## Unreleased changes
---
+++ hellohaskell/out/production/hellohaskell/Main.hs
@@ -0,0 +1,7 @@
+-- (이 기호는 주석입니다.)
+-- stack ghci 로 콘솔창 실행도 가능해요.
+-- :: stack ghci
+-- :: main
+ 
+import System.IO
+main = putStrLn "Hello, World!"
