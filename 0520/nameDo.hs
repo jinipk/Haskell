8aa06ac44073bb72ca0cafa490190398f0168cd1
jinipk <53362964+jinipk@users.noreply.github.com>
2021-05-20 10:44:29 +0900

    Add files via upload

---
+++ 0520/nameDo.hs
@@ -0,0 +1,8 @@
+nameDo :: IO ()
+nameDo = do 
+  putStr "What is your first name? "
+  first <- getLine
+  putStr "And your last name? "
+  last <- getLine
+  let full = first ++ " " ++ last
+  putStrLn ("Pleased to meet you, " ++ full ++ "!")
